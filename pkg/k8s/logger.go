b5d5cc04fe38ede0b5c4d340eefc14f2b846bdb0
Doug Edgar <dedgar@redhat.com>
2020-04-07 16:47:38 -0700

    catching up before rename

--- pkg/k8s/logger.go
+++ pkg/k8s/logger.go
@@ -41,7 +41,7 @@ func LoggerDaemonSet(m *managedv1alpha1.Logger) *appsv1.DaemonSet {
 						},
 					},
 					Containers: []corev1.Container{{
-						Image: "quay.io/dedgar/pod-logger:v0.0.7",
+						Image: "quay.io/dedgar/pod-logger:v0.0.8",
 						Name:  "logger",
 						SecurityContext: &corev1.SecurityContext{
 							Privileged: &privileged,
